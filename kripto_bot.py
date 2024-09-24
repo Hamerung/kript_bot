b7719e5c5c93e7bf2eefd22cc7a8e336bc340f22
Aleksey Pribytkov <hamerungg91@gmail.com>
2024-09-24 09:36:15 +0500

    апи для котиков

--- kripto_bot.py
+++ kripto_bot.py
@@ -4,6 +4,7 @@ from tg_bot import bot_token
 
 
 API_URL = 'https://api.telegram.org/bot'
+KOTIK = 'https://api.thecatapi.com/v1/images/search'
 max_counter = 100
 
 offset = -2
@@ -20,7 +21,9 @@ while counter < max_counter:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
             text = result['message']['text']
-            requests.get(f'{API_URL}{bot_token}/sendMessage?chat_id={chat_id}&text={text}')
+            url_cat = requests.get(KOTIK).json()[0]['url']
+            requests.get(f'{API_URL}{bot_token}/sendPhoto?chat_id={chat_id}&photo={url_cat}')
+            # requests.get(f'{API_URL}{bot_token}/sendMessage?chat_id={chat_id}&text={text}')
     
     time.sleep(1)
     counter += 1
